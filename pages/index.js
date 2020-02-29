34187df40db4c817af0f776fe113e3a215125c4c
diaosipeng <diao19980812@gmail.com>
2020-02-29 18:44:39 +0800

    news

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,21 @@
 function ClickTest(){
     document.write('恭喜你，成功了')
 }
+
+function info1(){
+    document.getElementById("jiaju").style.display="inline"
+    document.getElementById("house").style.display="none"
+    document.getElementById("second_home").style.display="none"
+}
+
+function info2(){
+    document.getElementById("house").style.display="inline"
+    document.getElementById("jiaju").style.display="none"
+    document.getElementById("second_home").style.display="none"
+}
+
+function info3(){
+    document.getElementById("second_home").style.display="inline"
+    document.getElementById("house").style.display="none"
+    document.getElementById("jiaju").style.display="none"
+}
